509701a59f4dbc3bcd957d1cfcf36251b35f6245
chasefc <chasecorreia2023@u.northwestern.edu>
2021-04-21 11:34:43 -0500

    HW5

--- HW4/nbproject/Makefile-variables.mk
+++ HW4/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=ME-433.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/ME-433.production.hex
+CND_ARTIFACT_NAME_default=HW4.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HW4.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=me-433.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/me-433.tar
+CND_PACKAGE_NAME_default=hw4.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/hw4.tar
